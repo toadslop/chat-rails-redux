b5dde077a58b0a5472817ef64d8f9c3f71a49d27
Brian Heise <bnheise@gmail.com>
2020-04-12 11:42:10 +0900

    seed file created

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,47 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# destroy database records
+puts 'Destroying all records...'
+Message.destroy_all
+Channel.destroy_all
+User.destroy_all
+puts '...finished'
+
+# stock data
+puts 'Making stock data...'
+User.create!(email: 'test@gmail.com', password: '123123')
+Channel.create!(name: 'Tokyo')
+puts '...finished'
+
+# random data
+
+puts 'generating fake users'
+number = '100000'
+10.times do
+  User.create!(
+    email: Faker::Internet.email,
+    password: number
+  )
+  number = (number.to_i + 1).to_s
+end
+puts '...done'
+
+puts 'Generating random channels...'
+5.times do
+  Channel.create!(name: Faker::Address.city)
+end
+puts '...done'
+
+puts 'Generating fake messages...'
+Channel.all.each do |channel|
+  rand(1..10).times do
+    Message.create!(
+      user: User.take,
+      channel: channel,
+      content: Faker::Lorem.sentence(word_count: rand(3..10))
+    )
+  end
+end
+puts '...done'
